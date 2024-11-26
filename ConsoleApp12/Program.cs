aca74f527c28b802d36faf6718bd0fb9264d9935
Дмитрий Стародуб <sergeistarodyb@gmail.com>
2024-11-26 20:29:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //Задача 1. Вывод  сообщений приветствия и прощания с использованием дополнительных методов.
+        //        static void Greet()
+        //        {
+        //            Console.WriteLine("Привет!");
+        //        }
+
+//        static void Farewell()
+//        {
+//            Console.WriteLine("Пока!");
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            Greet();
+//            Farewell();
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+//Задача 2. Нахождение суммы, разности, произведения и частного двух целых чисел с использованием дополнительных методов
+//            Console.Write("Введите x: ");
+//            double x = Convert.ToDouble(Console.ReadLine());
+
+//            double y;
+//            if (x < 5)
+//            {
+//                y = Math.Pow(x, 2) + 1;
+//            }
+//            else
+//            {
+//                y = 3 * x - 2;
+//            }
+
+//            Console.WriteLine($"При x={x}, значение y={y}");
+
+//            Console.Write("Введите y: ");
+//            double y2 = Convert.ToDouble(Console.ReadLine());
+
+//            double sum = Sum(x, y2);
+//            Console.WriteLine($"Сумма x и y = {sum}");
+
+//            Console.ReadLine();
+//        }
+
+//        static double Sum(double a, double b)
+//        {
+//            return a + b;
+//        }
+//    }
+//}
+//Задача 3. Вычисление значения функции
+//            Console.Write("Введите значение x: ");
+//            double x = Convert.ToDouble(Console.ReadLine());
+
+//            double y;
+//            if (x < 5)
+//            {
+//                y = (4 * Math.Pow(x, 2) + 1) / (x - 5);
+//            }
+//            else
+//            {
+//                y = 3 * Math.Pow(x, 2) - 2;
+//            }
+
+//            Console.WriteLine("Значение y: " + y);
+
+//            Console.Write("Введите значение для 2 числа: ");
+//            double secondValue = Convert.ToDouble(Console.ReadLine());
+
+//            double difference = CalculateDifference(x, secondValue);
+//            Console.WriteLine($"Разница между x и 2 числом: {difference}");
+
+//            Console.ReadLine();
+//        }
+
+//        static double CalculateDifference(double a, double b)
+//        {
+//            return a - b;
+//        }
+//    }
+//}
